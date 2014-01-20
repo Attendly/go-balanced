fac8a2ea0a07d49202051e5f2265e2ef116c6819
Kristian Puccio <kristian.puc>
2014-01-20 16:35:10 +1100

    added ability to add CC charge to customer

--- customer.go
+++ customer.go
@@ -1,6 +1,7 @@
 package balanced
 
 import (
+	"fmt"
 	"net/url"
 	"strconv"
 )
@@ -9,10 +10,12 @@ import (
 Customer represents a Balanced Customer
 */
 type Customer struct {
-	Id    string `json:"id,omitempty"`
-	Uri   string `json:"uri,omitempty"`
-	Name  string `json:"name,omitempty"`
-	Phone string `json:"phone,omitempty"`
+	Id     string `json:"id,omitempty"`
+	Uri    string `json:"uri,omitempty"`
+	Name   string `json:"name,omitempty"`
+	Phone  string `json:"phone,omitempty"`
+	Email  string `json:"email,omitempty"`
+	client *Client
 }
 
 type CustomerService struct {
@@ -57,9 +60,48 @@ func (s *CustomerService) Create(customer *Customer) (*Customer, error) {
 		return nil, err
 	}
 	c := new(Customer)
+	c.client = s.client
+
 	_, err = s.client.Do(req, c)
 	if err != nil {
 		return nil, err
 	}
 	return c, nil
+}
+
+func (c *Customer) AssosiateCard(cardToken string) error {
+	data := struct {
+		CardUri string `json:"card_uri"`
+	}{cardToken}
+
+	req, err := c.client.NewRequest("PUT", c.Uri, data)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.client.Do(req, c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Customer) DebitCustomer(amount int, description string) error {
+	u := fmt.Sprintf("%s/debits", c.Uri)
+
+	data := struct {
+		Amount      int    `json:"amount"`
+		Description string `json:"description"`
+	}{amount, description}
+
+	req, err := c.client.NewRequest("POST", u, data)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.client.Do(req, c)
+	if err != nil {
+		return err
+	}
+	return nil
 }
